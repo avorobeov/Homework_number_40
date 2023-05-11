695e4151417634c7c711ac10dc4bf0e7e9b72197
воробйов александр <avorobeov21@gmail.com>
2023-05-11 20:10:27 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_40/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Homework_number_40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+
+            int id = 1;
+            string name = "inna";
+            int lavel = 1;
+            bool isBanned = false;
+
+            database.Add(new Player(id, name, lavel, isBanned));
+            database.Add(new Player(id, name, lavel, isBanned));
+            database.Ban(id);
+            database.UnBan(id);
+            database.Remove(id);
+        }
+    }
+
+    class Player
+    {
+        public Player(int id, string name, int lavel, bool isBanned)
+        {
+            Id = id;
+            Name = name;
+            Level = lavel;
+            IsBanned = isBanned;
+        }
+
+        public int Id { get; private set; }
+        public int Level { get; private set; }
+        public bool IsBanned { get; private set; }
+        public string Name { get; private set; }
+
+        public void SetBannedStatus(bool isBanned)
+        {
+            IsBanned = isBanned;
+        }
+    }
+
+    class Database
+    {
+        private List<Player> _players = new List<Player>();
+
+        public void Add(Player player)
+        {
+            if (ContainsId(player.Id) == false)
+            {
+                _players.Add(player);
+            }
+        }
+
+        public void Remove(int id)
+        {
+            Player Player = GetPlayer(id);
+
+            if (Player != null)
+            {
+                _players.Remove(Player);
+            }
+        }
+
+        public void Ban(int id)
+        {
+            Player Player = GetPlayer(id);
+
+            if (Player != null)
+            {
+                Player.SetBannedStatus(true);
+            }
+        }
+
+        public void UnBan(int id)
+        {
+            Player Player = GetPlayer(id);
+
+            if (Player != null)
+            {
+                Player.SetBannedStatus(false);
+            }
+        }
+
+        private Player GetPlayer(int id)
+        {
+            for (int i = 0; i < _players.Count; i++)
+            {
+                if (_players[i].Id == id)
+                {
+                    return _players[i];
+                }
+            }
+
+            return null;
+        }
+
+        private bool ContainsId(int id)
+        {
+            Player Player = GetPlayer(id);
+
+            if (Player == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Homework_number_40/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Homework_number_40")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Homework_number_40")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("93d2268e-8cd1-43f6-ae35-7ad1239b30cd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
